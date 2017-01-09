74099e19f1a65ca5e445705afa07646a91d35e36
Hilmi Hassan <mhilmi1117@gmail.com>
2017-01-09 16:19:16 +0800

    add font awesome + avatar component

--- imports/ui/core/sidebar/avatar.jsx
+++ imports/ui/core/sidebar/avatar.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Avatar = () => {
   return (
-    <div className="avatar bg-orange">
-      Avatar Component
+    <div id="avatar" className="bg-orange container-fluid">
+      <div className="row">
+        <div className="col-3"><img src="http://lorempixel.com/400/200" alt="" /></div>
+        <div id="avatar-info" className="col-9 pl-0">
+          <div className="name">Muhammad Hilmi</div>
+          <div className="position">Software Developer</div>
+        </div>
+      </div>
     </div>
   );
 };
